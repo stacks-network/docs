5220818c60074d2b97ae884222c74c725c450c2d
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-07-20 14:51:48 -0500

    feat: faq

--- src/common/mdx.ts
+++ src/common/mdx.ts
@@ -2,11 +2,45 @@ import { MDXComponents } from '@components/mdx/mdx-components';
 import { cliReferenceData } from '@common/_data/cliRef';
 import renderToString from 'next-mdx-remote/render-to-string';
 import CLARITY_REFERENCE from '@common/_data/clarityRef.json';
+import FAQ_JSON from '@common/_data/faqs.json';
+import TurndownService from 'turndown';
+
+const remarkPlugins = [
+  require('remark-squeeze-paragraphs'),
+  require('remark-external-links'),
+  require('remark-emoji'),
+  require('remark-images'),
+  require('remark-unwrap-images'),
+  require('remark-normalize-headings'),
+  require('remark-slug'),
+];
 
 export const wrapValueInTicks = value => '`' + value.replace('`', '').replace('`', '') + '`';
 
 export const convertRemoteDataToMDX = async (arr: any[], key: string) =>
-  Promise.all(arr.map(entry => renderToString(entry[key], MDXComponents)));
+  Promise.all(arr.map(entry => renderToString(entry[key], MDXComponents, { remarkPlugins })));
+
+export const convertFaqAnswersToMDX = async () => {
+  const turndownService = new TurndownService();
+  // we convert html to markdown so we can process it with remark,
+  // eg external links open in new window
+  const md = FAQ_JSON.faqs.map(faq => ({
+    ...faq,
+    answer: turndownService.turndown(faq.answer),
+  }));
+  // convert it to MDX with next-mdx-remote
+  const answers = await convertRemoteDataToMDX(md, 'answer');
+  const faqs = FAQ_JSON.faqs.map((faq, index) => ({
+    ...faq,
+    answer: answers[index],
+  }));
+
+  return {
+    props: {
+      mdx: faqs,
+    },
+  };
+};
 
 export const convertBlockstackCLIUsageToMdx = async () => {
   const results = await convertRemoteDataToMDX(cliReferenceData, 'usage');
