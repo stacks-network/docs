9d5f7c0a803ad7ae7338af14f702c96dc0c0fc93
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-08-10 20:19:42 -0500

    feat: use fathom, meta improvements

--- src/components/page-top.tsx
+++ src/components/page-top.tsx
@@ -30,11 +30,11 @@ export const PageTop: React.FC<PageTopProps> = React.memo(
               headings,
             })}
           </H1>
-          {isHome ? (
-            <Box width="100%" maxWidth="208px">
-              <Search />
-            </Box>
-          ) : null}
+          {/*{isHome ? (*/}
+          {/*  <Box width="100%" maxWidth="208px">*/}
+          {/*    <Search />*/}
+          {/*  </Box>*/}
+          {/*) : null}*/}
         </Flex>
         {description ? (
           <Box mt="40px !important">
