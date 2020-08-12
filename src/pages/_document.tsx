e6a25e804fb7f5d23a99afcb33124a85b64de965
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-08-12 12:33:47 -0500

    fix: status checker, other misc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,45 +40,36 @@ export default class MyDocument extends Document<DocumentProps> {
       <Html lang="en">
         <Head />
         <body>
-          <style
-            type="text/css"
-            dangerouslySetInnerHTML={{
-              __html: `
-@font-face {
-  font-family: 'Soehne Mono';
-  src: url('/static/fonts/soehne-mono-web-buch.woff2') format('woff2'),
-       url('/static/fonts/soehne-mono-web-buch.woff') format('woff');
-  font-weight: 400;
-  font-display: swap;
-  font-style: normal;
-}
-@font-face {
-  font-family: 'Soehne';
-  src: url('/static/fonts/soehne-web-buch.woff2') format('woff2'),
-       url('/static/fonts/soehne-web-buch.woff') format('woff');
-  font-weight: 400;
-  font-display: swap;
-  font-style: normal;
-}
-@font-face {
-    font-family: 'Soehne';
-  src: url('/static/fonts/soehne-web-kraftig_1.woff2') format('woff2'),
-       url('/static/fonts/soehne-web-kraftig_1.woff') format('woff');
-  font-weight: 500;
-  font-display: swap;
-  font-style: normal;
-}
-@font-face {
-    font-family: 'Soehne';
-  src: url('/static/fonts/soehne-web-halbfett_1.woff2') format('woff2'),
-       url('/static/fonts/soehne-web-halbfett_1.woff') format('woff');
-  font-weight: 600;
-  font-display: swap;
-  font-style: normal;
-}
-`,
-            }}
+          <link
+            rel="preload"
+            href="/static/fonts/soehne-mono-web-buch.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="true"
           />
+          <link
+            rel="preload"
+            href="/static/fonts/soehne-web-buch.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="true"
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/soehne-web-kraftig_1.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="true"
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/soehne-web-halbfett_1.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="true"
+          />
+          <link rel="preload" href="/static/fonts.css" as="style" />
+
           <script
             dangerouslySetInnerHTML={{
               __html: `(function() {
@@ -93,7 +84,7 @@ export default class MyDocument extends Document<DocumentProps> {
               })()`,
             }}
           />
-          <link rel="preconnect" href="https://bh4d9od16a-dsn.algolia.net" crossOrigin="true" />
+          {/*<link rel="preconnect" href="https://bh4d9od16a-dsn.algolia.net" crossOrigin="true" />*/}
           <link rel="preconnect" href="https://cdn.usefathom.com" crossOrigin="true" />
           <Main />
           <NextScript />
