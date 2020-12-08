fcca3f7a291a0e2ce7a3381c8e4dce4525d24376
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-12-08 11:50:36 -0600

    fix: IconButton, search styles

--- src/components/search-button.tsx
+++ src/components/search-button.tsx
@@ -1,23 +1,22 @@
 import React, { forwardRef, Ref } from 'react';
-import { color } from '@stacks/ui';
+import { IconButton } from '@stacks/ui';
 import { LinkProps } from '@components/typography';
-import { IconButton } from '@components/icon-button';
-import { useAppState } from '../common/hooks/use-app-state';
+
+import { useAppState } from '@common/hooks/use-app-state';
 import { MagnifyingGlass } from './icons/magnifying-glass';
 
 export const SearchButton = forwardRef((props: LinkProps, ref: Ref<HTMLDivElement>) => {
-  const { setState, searchModal } = useAppState();
+  const { setState } = useAppState();
 
   return (
     <IconButton
+      display={['grid', 'grid', 'none']}
       onClick={() => setState(state => ({ ...state, searchModal: 'open' }))}
       title="Search docs"
-      px="base"
-      {...props}
       ref={ref}
-    >
-      {searchModal}
-      <MagnifyingGlass size={18} color={color('text-title')} />
-    </IconButton>
+      iconSize="18px"
+      icon={MagnifyingGlass}
+      {...props}
+    />
   );
 });
