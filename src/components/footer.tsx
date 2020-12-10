367c9a05498c0a44fcb73ce1e384c507fc0c497f
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-12-09 19:13:19 -0600

    fix: add vercel banner

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { space } from '@stacks/ui';
+import { Box, Flex, space } from '@stacks/ui';
 import { Pagination } from '@components/pagination';
 import { Section, SectionWrapper } from '@components/common';
 import { FeedbackSection } from '@components/feedback';
+import { VercelLogo } from '@components/vercel';
+import { Caption } from '@components/typography';
 
 const Footer = ({ hidePagination, ...rest }: any) => {
   return (
@@ -10,6 +12,24 @@ const Footer = ({ hidePagination, ...rest }: any) => {
       <SectionWrapper>
         <Pagination />
         <FeedbackSection />
+        <Flex
+          justifyContent={['flex-end', 'flex-end', 'flex-start']}
+          mt={['extra-loose', 'extra-loose', 'loose']}
+          alignItems="center"
+        >
+          <Caption mt={['loose', 'loose', 'unset']} mr="tight">
+            Powered by
+          </Caption>
+          <Box
+            mt={['loose', 'loose', 'unset']}
+            href="https://vercel.com/?utm_source=blockstack&utm_campaign=oss"
+            as="a"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <VercelLogo opacity={0.85} />
+          </Box>
+        </Flex>
       </SectionWrapper>
     </Section>
   );
