2e9c88a625e162cd8070211691c388b4e1b5c624
Alexander Graebe <alex@blockstack.com>
2020-12-21 15:29:32 +0000

    fix: set width to 860px to optimize reading experience

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,6 @@
 export const SIDEBAR_WIDTH = 208;
 export const TOC_WIDTH = 208;
-export const CONTENT_MAX_WIDTH = 1175;
+export const CONTENT_MAX_WIDTH = 860;
 
 export const SHIKI_THEME = 'Material-Theme-Default';
 export const THEME_STORAGE_KEY = 'theme';
