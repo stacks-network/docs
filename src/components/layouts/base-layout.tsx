d1f9ee20e969af8e6923aac18282ecc2a2cb8377
pgray-hiro <83602086+pgray-hiro@users.noreply.github.com>
2021-05-12 08:49:31 -0400

    Update src/components/layouts/base-layout.tsx
    Co-authored-by: Thomas Osmonson <aulneau@users.noreply.github.com>

--- src/components/layouts/base-layout.tsx
+++ src/components/layouts/base-layout.tsx
@@ -36,7 +36,7 @@ const BaseLayout: React.FC<{ isHome?: boolean }> = ({ children }) => {
               <Flex
                 flexDirection={['column', 'column', 'row', 'row']}
                 maxWidth={`${CONTENT_MAX_WIDTH}px`}
-                pl={4}
+                pl="base"
                 mx="auto"
                 flexGrow={1}
               >
