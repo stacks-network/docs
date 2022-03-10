ed47f0907a26ad636faac556704b577c2737e11f
Jonathon Hammond <jonathon@pixel8.io>
2022-03-10 11:45:51 -0500

    fix for api status page

--- src/pages/en/api/status.js
+++
@@ -1,9 +0,0 @@
-export default async (req, res) => {
-  const response = await fetch('http://status.test-blockstack.com/json');
-  const json = await response.json();
-  if (json) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(json.blockRateStatus));
-  }
-};
