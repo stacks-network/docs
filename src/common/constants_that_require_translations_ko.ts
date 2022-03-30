c7dedc2e62ba3f1f466c2226e446cd77d3d91837
Jonathon Hammond <jonathon@pixel8.io>
2022-03-30 12:53:10 -0400

    New translations constants_that_require_translations.ts (Korean)

--- src/common/constants_that_require_translations_ko.ts
+++ src/common/constants_that_require_translations_ko.ts
@@ -1,5 +1,5 @@
 // Constants that will require translations into different languages should go here
-export const EDIT_PAGE_IN_GITHUB = 'Edit this page on GitHub';
+export const EDIT_PAGE_IN_GITHUB = 'Edit this page in English on GitHub';
 export const COMMUNITY = 'Community';
 export const LANGUAGES = 'Languages';
 export const LEARNING_RESOURCES = 'Learning Resources';
