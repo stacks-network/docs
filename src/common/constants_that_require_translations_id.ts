d2e3267d038afffe82207bf2c0e1b8dea1ccf8b4
bot-translations <104575519+bot-translations@users.noreply.github.com>
2022-04-28 20:19:23 +0200

    New translations constants_that_require_translations.ts (Indonesian)

---
+++ src/common/constants_that_require_translations_id.ts
@@ -0,0 +1,8 @@
+// Constants that will require translations into different languages should go here
+export const EDIT_PAGE_IN_GITHUB = "Edit halaman ini dalam Bahasa Indonesia di GitHub";
+export const COMMUNITY = "Komunitas";
+export const LANGUAGES = "Bahasa";
+export const LEARNING_RESOURCES = "Bahan Belajar";
+export const SEARCH_DOCS = "Cari dokumen";
+export const TUTORIALS = "Tutorial";
+export const HOME = "Beranda";
