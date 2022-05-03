35b7116010054bb752dd1a5e6698ef4213a2d2d2
bot-translations <104575519+bot-translations@users.noreply.github.com>
2022-05-03 02:39:29 +0200

    New translations constants_that_require_translations.ts (Korean)

---
+++ src/common/constants_that_require_translations_ko.ts
@@ -0,0 +1,8 @@
+// Constants that will require translations into different languages should go here
+export const EDIT_PAGE_IN_GITHUB = 'Edit this page in English on GitHub';
+export const COMMUNITY = 'Community';
+export const LANGUAGES = 'Languages';
+export const LEARNING_RESOURCES = 'Learning Resources';
+export const SEARCH_DOCS = 'Search docs';
+export const TUTORIALS = 'Tutorials';
+export const HOME = 'Home';
