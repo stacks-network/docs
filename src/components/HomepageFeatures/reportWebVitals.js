c68c1491e8f9183dc0ebdbd192dfa8860a06d0bd
Alejandro Criado-Pérez <alejandro@criadoperez.com>
2022-08-11 18:30:07 +0200

    AnalyticsID (#1510)

--- src/components/HomepageFeatures/reportWebVitals.js
+++ src/components/HomepageFeatures/reportWebVitals.js
@@ -17,8 +17,8 @@ function sendToAnalytics(metric, options) {
   );
 
   const body = {
-    dsn: options.analyticsId, // qPgJqYH9LQX5o31Ormk8iWhCxZO
-    id: QmUAisL6QycUbijho9eqm4hh1ZNf7cuCCCrraMx4oUCPTa, //metric.id, // v2-1653884975443-1839479248192
+    dsn: QmUAisL6QycUbijho9eqm4hh1ZNf7cuCCCrraMx4oUCPTa, //options.analyticsId, // qPgJqYH9LQX5o31Ormk8iWhCxZO
+    id: metric.id, // v2-1653884975443-1839479248192
     page, // /blog/[slug]
     href: location.href, // https://my-app.vercel.app/blog/my-test
     event_name: metric.name, // TTFB
